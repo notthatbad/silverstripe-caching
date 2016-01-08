9b6f734a652e3d238a034cf0a20d0d96a7bf1bca
Eduard Malyj <eduard.malyj@gmail.com>
2016-01-08 11:32:05 +0100

    Improve functional tests

--- tests/unittests/CacheHelperTest.php
+++ tests/unittests/CacheHelperTest.php
@@ -16,4 +16,27 @@ class CacheHelperTest extends SapphireTest {
         $serializer = CacheHelper::get_serializer();
         $this->assertTrue(ClassInfo::classImplements($serializer->class, 'ICacheSerializer'));
     }
+
+    public function testCacheFunction() {
+        $call = CacheHelper::cache_function('CacheHelperTest::helperFunction', 1, 'test');
+        sleep(2);
+        $cache = CacheHelper::cache_function('CacheHelperTest::helperFunction', 1, 'test');
+        $this->assertEquals($call, $cache);
+    }
+
+    public function testCacheFunctionWithVariousParameter() {
+        $first = CacheHelper::cache_function('CacheHelperTest::helperFunction', 1, 'string');
+        $second = CacheHelper::cache_function('CacheHelperTest::helperFunction', 2, 'string');
+        $this->assertNotEquals($first, $second);
+    }
+
+    public static function helperFunction($int, $string) {
+        $result = $int.$string.time();
+        return $result;
+    }
+
+    public function testGetCache() {
+        $cache = CacheHelper::get_cache();
+        $this->assertEquals(ClassInfo::class_name($cache), 'Zend_Cache_Frontend_Function');
+    }
 }
